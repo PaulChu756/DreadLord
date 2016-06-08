a0c7fde7b9a9167d98e2147acc8428840de1b04a
Paul Chu <paulchu756@gmail.com>
2016-06-08 18:24:15 -0500

    Added asset package and also added simple movement.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -3,5 +3,33 @@ using System.Collections;
 
 public class CharacterController : MonoBehaviour
 {
+    public float m_movement = 10.0f;
 
+    void Move()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * m_movement * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * m_movement * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * m_movement * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * m_movement * Time.deltaTime);
+        }
+    }
+
+    void Update()
+    {
+        Move();
+    }
 }
