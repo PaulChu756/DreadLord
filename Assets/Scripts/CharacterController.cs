7ae4a572ebe2eedf2c25f27c81324f3ab6de69f1
Paul Chu <paulchu756@gmail.com>
2016-06-13 10:25:24 -0500

    Character Controller, character moves smooth like butter.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,12 +4,15 @@ using System.Collections;
 
 public class CharacterController : MonoBehaviour
 {
-    [SerializeField]
+    // Serializefield shows variable inside inspector.
+    [SerializeField] 
     private float m_Speed = 20.0f;
     [SerializeField]
     private float m_Jump = 20.0f;
+    [SerializeField]
+    private Vector3 m_Velocity = Vector3.zero;
+    [SerializeField]
     private Vector3 m_direction = Vector3.zero;
-    private bool isJump;
     public Rigidbody rb;
 
     void Start()
@@ -41,31 +44,26 @@ public class CharacterController : MonoBehaviour
         if(Input.GetKey(KeyCode.W))
         {
             m_direction = new Vector3(0, 0, 1);
-            transform.position += m_direction * m_Speed * Time.deltaTime;
-            //rb.MovePosition(transform.position + m_direction * m_Speed * Time.deltaTime);
+            m_Velocity += m_direction * m_Speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             m_direction = new Vector3(-1, 0, 0);
-            transform.position += m_direction * m_Speed * Time.deltaTime;
-            //rb.MovePosition(transform.position + m_direction * m_Speed * Time.deltaTime);
+            m_Velocity += m_direction * m_Speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             m_direction = new Vector3(1, 0, 0);
-            transform.position += m_direction * m_Speed * Time.deltaTime;
-            //rb.MovePosition(transform.position + m_direction * m_Speed * Time.deltaTime);
+            m_Velocity += m_direction * m_Speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             m_direction = new Vector3(0, 0, -1);
-            transform.position += m_direction * m_Speed * Time.deltaTime;
-            //rb.MovePosition(transform.position + m_direction * m_Speed * Time.deltaTime);
+            m_Velocity += m_direction * m_Speed * Time.deltaTime;
         }
-
-        rb.MovePosition(transform.position + m_direction * m_Speed * Time.deltaTime);
+        rb.MovePosition(transform.position + m_Velocity * Time.deltaTime); // Bread and butter
     }
 }
