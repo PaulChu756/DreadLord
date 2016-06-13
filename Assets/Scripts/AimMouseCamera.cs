7f16cf40aeca66b3831eba733a85e926307324fe
Paul Chu <paulchu756@gmail.com>
2016-06-13 16:29:54 -0500

    Made camera rotate.

--- Assets/Scripts/AimMouseCamera.cs
+++ Assets/Scripts/AimMouseCamera.cs
@@ -7,4 +7,29 @@ public class AimMouseCamera : MonoBehaviour
     // except the rotation is controlled by the mouse, 
     // which then points the character in whatever direction the camera is facing.
 
+    public GameObject target;
+    [SerializeField]
+    private float rotateSpeed = 5;
+    [SerializeField]
+    private Vector3 offSet;
+
+    void Start()
+    {
+        offSet = target.transform.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+        // use Get Axis Mouse X, to rotate on X-Axis
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.transform.Rotate(0, horizontal, 0);
+
+        //// give an offset same direction and put camera behind target.
+        //float desiredAngle = target.transform.eulerAngles.y;
+        //Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        //transform.position = target.transform.position - (rotation * offSet);
+
+        //transform.LookAt(target.transform);
+    }
+
 }
