22cc55b7d83118f7f8c04009cbb58ee4455b9cc1
Paul Chu <paulchu756@gmail.com>
2016-06-14 14:04:15 -0500

    Need to add friction

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -6,11 +6,11 @@ public class CharacterController : MonoBehaviour
 {
     // Serializefield shows variable inside inspector.
     [SerializeField] 
-    private float m_Speed = 20.0f;
+    private float m_MovementSpeed = 1.0f;
     [SerializeField]
-    private float m_Turn = 20.0f;
+    private float m_TurnSpeed = 10.0f;
     [SerializeField]
-    private float m_Jump = 100.0f;
+    private float m_Jump = 10.0f;
     [SerializeField]
     private Vector3 m_Velocity = Vector3.zero;
     [SerializeField]
@@ -40,39 +40,36 @@ public class CharacterController : MonoBehaviour
 
     void Movement()
     {
+        // Forward
         if (Input.GetKey(KeyCode.W))
         {
             m_direction = new Vector3(0, 0, 1);
-            m_Velocity += m_direction * m_Speed * Time.deltaTime;
-            transform.Rotate(0,  m_Turn * Time.deltaTime, 0);
-            //m_Rotate += m_direction * m_Turn * Time.deltaTime;
+            m_Velocity += m_direction * m_MovementSpeed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            m_direction = new Vector3(-1, 0, 0);
-            m_Velocity += m_direction * m_Speed * Time.deltaTime;
-            transform.Rotate(0, m_Turn * Time.deltaTime, 0);
-            //m_Rotate += m_direction * m_Turn * Time.deltaTime;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            m_direction = new Vector3(1, 0, 0);
-            m_Velocity += m_direction * m_Speed * Time.deltaTime;
-            transform.Rotate(0, m_Turn * Time.deltaTime, 0);
-            //m_Rotate += m_direction * m_Turn * Time.deltaTime;
-        }
-
+        // Backward
         if (Input.GetKey(KeyCode.S))
         {
             m_direction = new Vector3(0, 0, -1);
-            m_Velocity += m_direction * m_Speed * Time.deltaTime;
-            transform.Rotate(0, m_Turn * Time.deltaTime, 0);
-            //m_Rotate += m_direction * m_Turn * Time.deltaTime;
+            m_Velocity += m_direction * m_MovementSpeed * Time.deltaTime;
         }
-        //Quaternion rotate = Quaternion.Euler(m_Rotate * Time.deltaTime);
-        rb.MovePosition(transform.position + m_Velocity * Time.deltaTime);
+
+        // Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_direction = new Vector3(-1, 0, 0);
+            m_Velocity += m_direction * m_MovementSpeed * Time.deltaTime;
+            //transform.Rotate(Vector3.up * -m_TurnSpeed * Time.deltaTime);
+        }
+
+        // Right
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_direction = new Vector3(1, 0, 0);
+            m_Velocity += m_direction * m_MovementSpeed * Time.deltaTime;
+            //transform.Rotate(Vector3.up * m_TurnSpeed * Time.deltaTime);
+        }
+        rb.MovePosition(rb.position + m_Velocity * Time.deltaTime);
         //rb.MoveRotation(rb.rotation * rotate);
     }
 }
