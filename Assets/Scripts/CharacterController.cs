288a99a160e2476b80659405719ce9136be6346c
Paul Chu <lordbabypanda@gmail.com>
2016-06-16 15:47:36 -0500

    Simple rotate

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,20 +5,25 @@ using System.Collections;
 public class CharacterController : MonoBehaviour
 {
     // Serializefield shows variable inside inspector.
+
+    // Completed
     [SerializeField] 
     private float m_MovementSpeed = 10.0f;
-    [SerializeField]
-    private float m_TurnSpeed = 10.0f;
     [SerializeField]
     private float m_Jump = 10.0f;
     [SerializeField]
     private float m_Friction = 2.5f;
     [SerializeField]
     private Vector3 m_Velocity = Vector3.zero;
+
+    // Testing
+    [SerializeField]
+    private float m_TurnSpeed = 10.0f;
     [SerializeField]
     private Vector3 m_Direction = Vector3.zero;
     [SerializeField]
     private Vector3 m_AngleVel = Vector3.zero;
+
     public Rigidbody rb;
 
     void Start()
@@ -54,30 +59,38 @@ public class CharacterController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            m_Velocity += transform.forward * m_MovementSpeed * Time.deltaTime;
-            m_Direction = m_Velocity.normalized;
+            m_Velocity += transform.forward = new Vector3(0, 0, 1) * m_MovementSpeed * Time.deltaTime;
+
+            //m_Direction = m_Velocity.normalized;
+            //Rotate = Quaternion.LookRotation(m_Direction);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            m_Velocity -= transform.forward * m_MovementSpeed * Time.deltaTime;
-            m_Direction = m_Velocity.normalized;
+            m_Velocity += transform.forward = new Vector3(0, 0, -1) * m_MovementSpeed * Time.deltaTime;
+
+            //m_Direction -= m_Velocity.normalized;
+            //Rotate = Quaternion.LookRotation(m_Direction);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            m_Velocity -= transform.right * m_MovementSpeed * Time.deltaTime;
-            m_Direction = m_Velocity.normalized;
-            Rotate = Quaternion.LookRotation(m_Direction);
+            m_Velocity += transform.forward = new Vector3(-1, 0, 0) * m_MovementSpeed * Time.deltaTime;
+
+            //m_Direction = m_Velocity.normalized;
+            //Rotate = Quaternion.LookRotation(m_Direction);
+            //transform.rotation = Rotate;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            m_Velocity += transform.right * m_MovementSpeed * Time.deltaTime;
-            m_Direction = m_Velocity.normalized;
-            Rotate = Quaternion.LookRotation(m_Direction);
+            m_Velocity += transform.forward = new Vector3(1,0,0) * m_MovementSpeed * Time.deltaTime;
+            //m_Direction = m_Velocity.normalized;
+            //Rotate = Quaternion.LookRotation(m_Direction);
+            //transform.rotation = Rotate;
         }
         rb.MovePosition(rb.position + m_Velocity * Time.deltaTime);
-        rb.MoveRotation(rb.rotation * Rotate);
+        
+        //rb.MoveRotation(rb.rotation * Rotate);
     }
 }
